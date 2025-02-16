8d7d4c247c1f251fa05685af196b1a9cd3339b0b
winniehhy <yoyowinnie3@gmail.com>
2025-02-17 00:43:11 +0800

    currently data store in management portal first

--- iris.js
+++ iris.js
@@ -1,3 +1,35 @@
+// const irisnative = require("intersystems-iris-native");
+
+// function main() {
+//     // Database credentials
+//     const dbConfig = {
+//         host: "localhost",
+//         port: 1972,
+//         ns: "USER",
+//         user: "_SYSTEM",
+//         pwd: "121314",
+//     };
+
+//     try {
+//         // Connect to IRIS
+//         const connection = irisnative.createConnection(dbConfig);
+//         console.log("Hello World! You have successfully connected to InterSystems IRIS.");
+        
+//         // Close connection
+//         connection.close();
+//     } catch (error) {
+//         console.error("Failed to connect to IRIS:", error.message);
+//     }
+// }
+
+// main();
+
+
+
+
+
+/*------------to test connection database with intersystems ( dun delete here first)----------------*/
+
 const irisnative = require('intersystems-iris-native')
 
 function main()
@@ -13,10 +45,10 @@ function main()
     const connection = irisnative.createConnection({host: ip, port: port, ns: namespace, user: username, pwd: password})
     console.log("Hello World! You have successfully connected to InterSystems IRIS.")
 
-    // Create an InterSystems IRIS native object
+    // // Create an InterSystems IRIS native object
     const irisNative = connection.createIris()
 
-    // Store data natively into a global using the InterSystems IRIS native object
+    // // Store data natively into a global using the InterSystems IRIS native object
     irisNative.set(8888, "^testglobal", "1");
     globalValue = irisNative.get("^testglobal", "1");
     console.log("The value of ^testglobal(1) is " + globalValue);
